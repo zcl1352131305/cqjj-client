248919201a19da540dd06aa31c535ece21480ba7
邹朝亮 <1352131305@qq.com>
2018-05-04 16:17:22 +0800

    完成支出记账功能

--- routes/wechat/merchant/account.js
+++ routes/wechat/merchant/account.js
@@ -30,7 +30,7 @@ router.get('/sale', function (req, res, next) {
             logger.error("----"+JSON.stringify(data));
             returnObj.transaction = data.result;
             returnObj.target = req.query.target;
-            res.render(ejsPrefix+"transaction_account",returnObj);
+            res.render(ejsPrefix+"sale_account",returnObj);
         });
     }
     else{
@@ -39,7 +39,7 @@ router.get('/sale', function (req, res, next) {
 
         };
         returnObj.target = req.query.target;
-        res.render(ejsPrefix+"transaction_account",returnObj);
+        res.render(ejsPrefix+"sale_account",returnObj);
     }
 
 });
@@ -60,7 +60,7 @@ router.post('/sale/saveOrUpdate', function (req, res, next) {
 
 //销售记账成功
 router.get('/sale/toAccountSuccess', function (req, res, next) {
-    res.render(ejsPrefix+"transaction_account_success",{
+    res.render(ejsPrefix+"sale_account_success",{
         id:req.query.id
     });
 });
@@ -91,6 +91,83 @@ router.get('/sale/deleteAccountRecord', function (req, res, next) {
     });
 });
 
+
+/** 支出记账  */
+//支出记账
+router.get('/spend', function (req, res, next) {
+    console.log(req.originalUrl)
+    var returnObj = {}
+    //是否是编辑
+    if(null != req.query.id && req.query.id != ''){
+        return Promise.try(function () {
+            return cRequest.sendRequest(req, res, {
+                url: constant.BASE_PATH + "/cqjjTrade/account/spend/get/"+req.query.id,
+                method: 'GET'
+            });
+        }).then(function (data) {
+            logger.error("----"+JSON.stringify(data));
+            returnObj.transaction = data.result;
+            returnObj.target = req.query.target;
+            res.render(ejsPrefix+"spend_account",returnObj);
+        });
+    }
+    else{
+        returnObj.transaction = {
+            id: '',
+
+        };
+        returnObj.target = req.query.target;
+        res.render(ejsPrefix+"spend_account",returnObj);
+    }
+
+});
+
+//支出记账保存
+router.post('/spend/saveOrUpdate', function (req, res, next) {
+    return Promise.try(function () {
+        return cRequest.sendRequest(req, res, {
+            url: constant.BASE_PATH + "/cqjjTrade/account/spend/saveOrUpdate",
+            body: req.body,
+            method: 'POST',
+            json:true
+        });
+    }).then(function (data) {
+        res.json(data);
+    });
+});
+
+//支出记账成功
+router.get('/spend/toAccountSuccess', function (req, res, next) {
+    res.render(ejsPrefix+"spend_account_success",{
+        id:req.query.id
+    });
+});
+
+//获取支出记账记录列表
+router.get('/spend/accountRecordList', function (req, res, next) {
+    return Promise.try(function () {
+        return cRequest.sendRequest(req, res, {
+            url: constant.BASE_PATH + "/cqjjTrade/account/spend/list",
+            qs: req.query,
+            method: 'GET'
+        });
+    }).then(function (data) {
+        logger.error("----"+JSON.stringify(data));
+        res.json(data);
+    });
+});
+
+//删除支出记账
+router.get('/spend/deleteAccountRecord', function (req, res, next) {
+    return Promise.try(function () {
+        return cRequest.sendRequest(req, res, {
+            url: constant.BASE_PATH + "/cqjjTrade/account/spend/remove/"+req.query.id,
+            method: 'DELETE'
+        });
+    }).then(function (data) {
+        res.json(data);
+    });
+});
 
 
 //记账记录列表页
