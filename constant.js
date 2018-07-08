93ff8a97a345e708d5bd42bb012b2b51f2f532f5
邹朝亮 <1352131305@qq.com>
2018-07-08 10:05:24 +0800

    优化

--- constant.js
+++ constant.js
@@ -2,10 +2,18 @@
  * Created by lhl on 2017/06/16.
  * 系统常量
  */
-//接口系统地址
+/*//接口系统地址
 var basePath = "http://127.0.0.1:8050";
 exports.BASE_PATH = basePath;
-exports.SERVER_PATH = "http://9qhyqk.natappfree.cc";
+exports.SERVER_PATH = "http://bnurv9.natappfree.cc";
 //本系统地址
-exports.SYSTEM_PATH = "http://zoucl.s1.natapp.cc";
+exports.SYSTEM_PATH = "http://zoucl.s1.natapp.cc";*/
+
+
+//接口系统地址
+var basePath = "http://sys-gate:8050";
+exports.BASE_PATH = basePath;
+exports.SERVER_PATH = "http://server.cqjjpt.com";
+//本系统地址
+exports.SYSTEM_PATH = "http://wx.cqjjpt.com";
 
